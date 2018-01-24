0db35519f2b959a544e737e75dd5947551646f37
upkanet <upkanet@gmail.com>
2018-01-24 22:16:13 +0100

    Downloaded Subs Count bugfix

--- sub.php
+++ sub.php
@@ -11,6 +11,6 @@ if(substr($dir, -1) != "/"){
 if(file_exists($dir)){
 	$shell = shell_exec("subbatch ".$dir);
 	$lines = preg_split("/((\r?\n)|(\r\n?))/", $shell);
-	echo json_encode(["type" => "success", "message" => count($lines)." subtitles downloaded"]);
+	echo json_encode(["type" => "success", "message" => (count($lines) - 1)." subtitles downloaded"]);
 }
 
