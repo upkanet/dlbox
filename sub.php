1913a7a37abe9f390382fab4f528f0e329698bd8
upkanet <upkanet@gmail.com>
2018-01-25 21:07:14 +0100

    Subliminal as subtitles provider

--- sub.php
+++ sub.php
@@ -9,8 +9,16 @@ if(substr($dir, -1) != "/"){
 }
 
 if(file_exists($dir)){
-	$shell = shell_exec("subbatch ".$dir);
+	$shell = shell_exec("subliminal download ".$dir." -l fr");
 	$lines = preg_split("/((\r?\n)|(\r\n?))/", $shell);
-	echo json_encode(["type" => "success", "message" => (count($lines) - 1)." subtitles downloaded"]);
-}
+	array_shift($lines);
+	array_pop($lines);
 
+	$message = $lines[0];
+	if(count($lines)>2){
+		$message .= '<br>'.end($lines);
+	}
+
+	echo json_encode(["type" => "info", 
+			"message" => $message]);
+}
